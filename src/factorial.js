7eb4a8e27b18c1253fb9a47d57c15e92a83f432b
Jansuk Emmanuel <jansabbas1@gmail.com>
2023-02-20 22:56:46 +0300

    modified to cater for factorial of one(1)

--- src/factorial.js
+++ src/factorial.js
@@ -1,6 +1,11 @@
 export class Factorial {
     getFactorialOf(number) {
         let testNumber = number;
+
+        if(testNumber == 1) {
+            return testNumber
+        }
+        
         for(let i = number - 1; i > 0; i--) {
             testNumber = testNumber * i;
         }
