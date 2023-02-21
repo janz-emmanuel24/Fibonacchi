ef4a69721f373a1494ecd0e4918d309e8501de51
Jansuk Emmanuel <jansabbas1@gmail.com>
2023-02-21 07:57:12 +0300

    added functionality for zero

--- src/factorial.js
+++ src/factorial.js
@@ -7,8 +7,8 @@ export class Factorial {
     getFactorialOf(number) {
         let testNumber = number;
 
-        if(testNumber == 1) {
-            return testNumber
+        if(testNumber == 1 || testNumber == 0) {
+            return 1
         }
         
         for(let i = number - 1; i > 0; i--) {
